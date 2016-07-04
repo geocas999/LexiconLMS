046f23e38463572473893a816fdde1306e12ef14
Anette <tonettant@yahoo.se>
2016-07-04 15:49:12 +0200

    Add  in Seed and changes in view users

--- LexiconLMS/Controllers/TeacherController.cs
+++ LexiconLMS/Controllers/TeacherController.cs
@@ -1,6 +1,7 @@
 ﻿using LexiconLMS.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -25,9 +26,29 @@ namespace LexiconLMS.Controllers
         }
         
         //2016-07-01 Anette - Link navbar Users
-        // GET: /Account/Users
-        public ActionResult Users()
+        //2016-07-04 Anette - Sort Order
+        // GET: /Teacher/Users
+        public ActionResult Users(string sortOrder)
         {
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewBag.DateSortParm = sortOrder == "Course.Name" ? "name_desc" : "Course.Name";
+            var users = from s in db.Users
+                           select s;
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    users = users.OrderByDescending(s => s.Name);
+                    break;
+                case "Course":
+                    users = users.OrderBy(s => s.Course);
+                    break;
+                case "phone_desc":
+                    users = users.OrderByDescending(s => s.Course);
+                    break;
+                default:
+                    users = users.OrderBy(s => s.Name);
+                    break;
+            }
             return View(db.Users.ToList());
         }
     }
