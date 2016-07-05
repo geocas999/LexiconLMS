bdd13fbfe7fdc6a09f4e4743c7704925088164ba
Anette <tonettant@yahoo.se>
2016-07-05 10:31:43 +0200

    Activityfix Name

--- LexiconLMS/Migrations/201607050823124_test2.Designer.cs
+++ LexiconLMS/Migrations/201607050823124_test2.Designer.cs
@@ -7,13 +7,13 @@ namespace LexiconLMS.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class Addedactivityname : IMigrationMetadata
+    public sealed partial class test2 : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(Addedactivityname));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(test2));
         
         string IMigrationMetadata.Id
         {
-            get { return "201607041322158_Added activity name"; }
+            get { return "201607050823124_test2"; }
         }
         
         string IMigrationMetadata.Source
--- LexiconLMS/Migrations/201607050823124_test2.cs
+++ LexiconLMS/Migrations/201607050823124_test2.cs
@@ -3,14 +3,16 @@ namespace LexiconLMS.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class Addedactivityname : DbMigration
+    public partial class test2 : DbMigration
     {
         public override void Up()
         {
+            AddColumn("dbo.Activities", "Name", c => c.String());
         }
         
         public override void Down()
         {
+            DropColumn("dbo.Activities", "Name");
         }
     }
 }
